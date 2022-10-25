f8cc9aba05f10c07059acf54b354a5551e2f9af8
Sandipan Das <87384281+sandidas@users.noreply.github.com>
2022-10-25 13:06:52 +0530

    Filter function completed

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
-const express = require('express'); // call express
-const app = express(); // use express 
-const cors = require('cors'); // call cors to use publicly
+const express = require("express"); // call express
+const app = express(); // use express
+const cors = require("cors"); // call cors to use publicly
 app.use(cors()); // call app to use cors
 //==
 const port = process.env.PORT || 5000; // declare port from where we can get access
 
+app.get("", (req, res) => {
+  res.send("your API is running.");
+});
 
+// select course file
+const courses = require("./data/course.json"); // select json file
 
+// get courses
+app.get("/courses/", (req, res) => {
+  res.send(courses);
+});
 
-app.get('', (req, res)=>{
-    res.send('your API is running.');
-})
+// get by id
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCourse = courses.find((course) => course._id === id);
+  res.send(selectedCourse);
+});
 
-
-
-
+// fetch category posts by category id (slug)
+app.get("/courses/category/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCategory = courses.filter((course) => course.category_slug === id);
+  res.send(selectedCategory);
+});
 
 // check server
-app.listen(port, ()=>{
-    console.log('Server running on port: ', port);
-})
+app.listen(port, () => {
+  console.log("Server running on port: ", port);
+});
